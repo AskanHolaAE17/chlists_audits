a32c7c087035b850d6bc2acc378723ec6339b4e8
AskanHolaAE17 <be.in.pair@gmail.com>
2020-02-28 00:32:43 +0200

    Create audit form with validation and to Audits page btns

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,7 +3,8 @@
 class Question < ApplicationRecord
 
   belongs_to :checklist, optional: true
-  
+
   validates :title,         presence: true, length: { minimum: 12, maximum: 40 }
   validates :description,   presence: true  
+  
 end
